2644e841c0b9bf87c3c1857905842f5b963412ae
Ershadul-Alam <kazisourav8@gmail.com>
2026-09-12 20:44:42 +0600

    Created Tech Cards

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import type { techTypes } from "../types/techTypes";
+import TechCards from "./TechCards";
 
 interface techProps{
     techPromise: Promise<techTypes[]>
@@ -7,13 +8,26 @@ interface techProps{
 
 const Technologies = ({techPromise}:  techProps) => {
     const technologies = use(techPromise);
-    console.log(technologies);
     return (
+        <div>
+
         <div className="container mx-auto">
             <h3 className="font-extrabold text-3xl">
             Explore the <span className="text-[#D74DAE]">Technologies</span>
             </h3>
             <p className="text-sm font-light">Pick one technology per category to build your ideal stack.</p>
+        </div>
+
+        <div className="grid grid-cols-3">
+        {
+            technologies.map((technology: techTypes)=> {
+                return <div key={technology.id}>
+                    <TechCards technology={technology} />
+                </div>
+            })
+        }
+        </div>
+
         </div>
     );
 };
